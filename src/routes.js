4d96d6bd035a588856316c6d8741a25717feda21
Gabriel Henrique <htm.gabriel@gmail.com>
2022-06-09 15:23:40 -0300

    adicionada as rotas de autenticação

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 import {Router} from "express"
+import authRoutes from "./routes/auth/authRoutes"
 
 const router = new Router()
 
-//router.use("/auth")
+router.use("/auth", authRoutes)
 
 export default router
