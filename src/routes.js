cb23865cf8d7271d4eb518a8cbc1c074ba43f907
Gabriel Henrique <htm.gabriel@gmail.com>
2022-06-14 15:46:06 -0300

    adicionado o /user nas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import {Router} from "express"
 import authRoutes from "./routes/auth/authRoutes"
+import userRoutes from "./routes/user/userRoutes.js"
 
 const router = new Router()
 
 router.use("/auth", authRoutes)
+router.use("/user", userRoutes)
 
 export default router
